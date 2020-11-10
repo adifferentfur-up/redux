0e5cd796a4dfdd2495ffd638f3a585ab748db5e3
Alex Garcia <adifferentfur.up@gmail.com>
2020-11-10 03:48:14 -0800

    immer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import {produce} from 'immer';
+
+let book = {title: "Harry Potter"};
+
+function publish(book){
+  return produce(book, draftBook => {
+    draftBook.isPublished = true;
+  });
+
+  console.log(book);
+  console.log(produce);
+}
