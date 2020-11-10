237f8838cfe5564a58ffb891573be5a2e02bb4b0
Alex Garcia <adifferentfur.up@gmail.com>
2020-11-10 03:11:43 -0800

    updating object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const person = {
+  name: "John",
+  address: {
+    city: "San Francisco",
+    country: "USA"
+  }
+};
+
+const updated = {
+  ...person,
+  address = {
+    ...person.address,
+    city: "New York"
+  },
+  name: "Bob"
+};
+
+console.log (person);
+console.log (updated);
