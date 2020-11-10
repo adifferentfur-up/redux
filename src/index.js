7eea191bd2358fb5d85b91c8dd05310ab5a9af70
Alex Garcia <adifferentfur.up@gmail.com>
2020-11-10 03:26:48 -0800

    updating array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const numbers = [1,2,3];
+
+const index = numbers.indexOf(2);
+const added = [
+  ...numbers.slice(0, index),
+  4,
+  ...numbers.slice(index)
+]
+console.log(added);
+
+const removed = [...numbers.filter(n => n!==2)];
+console.log(removed);
+
+const updated = [...numbers.map(n => n == 2 ? 20: n)];
+console.log(updated);
