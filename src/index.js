89d1f5e81232c3a41e0d9a7de371500431beae78
Alex Garcia <adifferentfur.up@gmail.com>
2020-11-10 02:48:29 -0800

    currying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import {compose, pipe} from "lodash/fp";
+let input ="   javascript  ";
+
+const trim = str => str.trim();
+const toLower = str => str.toLowerCase();
+const wrap = type => str => `<${type}>${str}</$type>`;
+
+const transform = pipe(trim, toLower, wrap("span"));
+
+console.log(transform(input));
